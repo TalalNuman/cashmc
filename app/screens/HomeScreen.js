8c997cc59ac72081494db2b6223df0df5513599e
Muhammad Talal Numan <75552390+TalalNuman@users.noreply.github.com>
2022-05-09 15:11:38 +0500

    home

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,4 +1,12 @@
-import {View, Image, Text, StatusBar, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  ScrollView,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import {
   heightPercentageToDP as hp,
@@ -9,19 +17,20 @@ import AppText from '../components/AppText';
 export default function Home() {
   return (
     <>
-      <View
-        style={{
-          height: hp(100),
-          backgroundColor: Colors.primary,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <StatusBar backgroundColor={Colors.primary} />
-        <Image
-          style={{height: 150, width: 150, marginVertical: hp(2)}}
-          source={require('../assets/icons/logo.png')}
-        />
-        <TouchableOpacity onPress={() => console.log('swój nick..')}>
+      <ScrollView>
+        <View
+          style={{
+            height: hp(100),
+            backgroundColor: Colors.primary,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <StatusBar backgroundColor={Colors.primary} />
+          <Image
+            style={{height: 150, width: 150, marginVertical: hp(2)}}
+            source={require('../assets/icons/logo.png')}
+          />
+
           <View
             style={{
               backgroundColor: '#4A2C2B',
@@ -36,46 +45,54 @@ export default function Home() {
               style={{marginRight: wp(3), marginLeft: wp(6)}}
               source={require('../assets/icons/box-arrow-in-le.png')}
             />
-            <AppText size={2.2} fontWeight={'600'} style={{color: Colors.sec}}>
+            <TextInput
+              style={{flex: 1, fontSize: hp(2.2), color: '#fff'}}
+              placeholderTextColor={Colors.sec}
+              placeholder={'Wpisz swój nick..'}
+            />
+            {/* <AppText size={2.2} fontWeight={'600'} style={{color: Colors.sec}}>
               Wpisz swój nick..
-            </AppText>
+            </AppText> */}
           </View>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => console.log('Kontynuuj')}>
+
+          <TouchableOpacity onPress={() => console.log('Kontynuuj')}>
+            <View
+              style={{
+                marginTop: hp(3),
+                backgroundColor: 'rgba(52, 52, 52, 0.8)',
+                width: wp(85),
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: hp(7),
+                borderRadius: 10,
+              }}>
+              <AppText
+                size={2.2}
+                fontWeight={'600'}
+                style={{color: Colors.white}}>
+                Kontynuuj
+              </AppText>
+            </View>
+          </TouchableOpacity>
           <View
             style={{
-              marginTop: hp(3),
-              backgroundColor: 'rgba(52, 52, 52, 0.8)',
-              width: wp(85),
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: hp(7),
-              borderRadius: 10,
+              flexDirection: 'row',
+              marginTop: hp(1.5),
+              marginRight: wp(16),
             }}>
-            <AppText
-              size={2.2}
-              fontWeight={'600'}
-              style={{color: Colors.white}}>
-              Kontynuuj
+            <AppText size={1.4} style={{color: Colors.gray}}>
+              Mam problem z zalogowaniem się.
             </AppText>
+            <TouchableOpacity onPress={() => console.log('Skontaktuj się')}>
+              <AppText
+                size={1.4}
+                style={{color: Colors.sec, marginLeft: wp(2)}}>
+                Skontaktuj się
+              </AppText>
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
-        <View
-          style={{
-            flexDirection: 'row',
-            marginTop: hp(1.5),
-            marginRight: wp(16),
-          }}>
-          <AppText size={1.4} style={{color: Colors.gray}}>
-            Mam problem z zalogowaniem się.
-          </AppText>
-          <TouchableOpacity onPress={() => console.log('Skontaktuj się')}>
-            <AppText size={1.4} style={{color: Colors.sec, marginLeft: wp(2)}}>
-              Skontaktuj się
-            </AppText>
-          </TouchableOpacity>
         </View>
-      </View>
+      </ScrollView>
       <Image
         style={{position: 'absolute', top: hp(2), left: -wp(7)}}
         source={require('../assets/icons/gold.png')}
